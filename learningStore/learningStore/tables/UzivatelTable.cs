e34ef72c4d8e0b1e6cc5aa6aca82e22a312e734b
Petr Kuča <pet.kuca@gmail.com>
2016-04-19 17:35:44 +0200

    Uklzení po marge

--- learningStore/learningStore/tables/UzivatelTable.cs
+++ learningStore/learningStore/tables/UzivatelTable.cs
@@ -11,18 +11,6 @@ namespace learningStore.tables
 {
     public class UzivatelTable :  TableProxy<Uzivatel>
     {
-<<<<<<< HEAD
-        #region SQL
-        private static String SQL_SELECT = "SELECT * FROM uzivatel;";
-        private static String SQL_SELECT_BYID = "SELECT * FROM uzivatel WHERE uzID=@id;";
-        private static String SQL_INSERT =
-            "INSERT INTO uzivatel (uzID, login, jmeno, prijmeni, email, registrace, role) VALUES (@id, @login, @jmeno, @prijmeni, @email, @registrace, @role);";
-        private static String SQL_UPDATE =
-            "UPDATE uzivatel SET login=@login, jmeno=@jmeno, prijmeni=@primeni, email=@email, role=@role WHERE uzID=@id;";
-        private static String SQL_DELETE =
-            "DELETE FROM uzivatel WHERE uzID=@id;";
-        #endregion
-=======
         private static String SQL_SELECT_BYID = "SELECT * FROM uzivatel WHERE uzID=@uzID";
 
         public UzivatelTable()
@@ -34,7 +22,7 @@ namespace learningStore.tables
             SQL_DELETE = "DELETE FROM uzivatel WHERE uzID=@uzID;";
 
         }
->>>>>>> Generika
+
 
         #region CRUD
         public override int Insert(Uzivatel t, DatabaseProxy pDb = null)
@@ -63,11 +51,7 @@ namespace learningStore.tables
             return row;
         }
 
-<<<<<<< HEAD
-        public Collection<Uzivatel> Select(DatabaseProxy pDb = null)
-=======
         public override Collection<Uzivatel> Select(DatabaseProxy pDb = null)
->>>>>>> Generika
         {
             Connecting(pDb);
 
@@ -82,30 +66,6 @@ namespace learningStore.tables
             return uzivatele;
         }
 
-<<<<<<< HEAD
-        public int Delete(Uzivatel uzivatel, DatabaseProxy pDb = null)
-        {
-            Database db;
-            if (pDb == null)
-            {
-                db = new Database();
-                db.Connect();
-            }
-            else
-            {
-                db = (Database)pDb;
-            }
-
-            SqlCommand command = db.CreateCommand(SQL_DELETE);
-
-            command.Parameters.AddWithValue("@id", uzivatel.UzId);
-            int row = db.ExecuteNonQuery(command);
-
-            if (pDb == null)
-            {
-                db.Close();
-            }
-=======
         public override int Delete(Uzivatel t, DatabaseProxy pDb = null)
         {
             Connecting(pDb);
@@ -116,18 +76,11 @@ namespace learningStore.tables
             int row = db.ExecuteNonQuery(command);
 
             Disconnecting(pDb);
->>>>>>> Generika
-
             return row;
         }
         #endregion
 
-<<<<<<< HEAD
-        #region Detail uživatele
-        public Uzivatel SelectByID(int id, DatabaseProxy pDb = null)
-=======
         public Uzivatel SelectByID(int ID, DatabaseProxy pDb = null)
->>>>>>> Generika
         {
             Connecting(pDb);
 

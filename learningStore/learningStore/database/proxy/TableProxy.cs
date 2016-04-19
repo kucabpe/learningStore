f0e1ede96b1d280efd06f2b92868aefb2b3ba1d4
Petr Kuča <pet.kuca@gmail.com>
2016-04-19 13:19:38 +0200

    Test generika

---
+++ learningStore/learningStore/database/proxy/TableProxy.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using learningStore.database.mssql;
+using learningStore.database.proxy;
+
+namespace learningStore.tables
+{
+    public abstract class TableProxy<T>
+    {
+        protected Database db;
+
+        #region SQL
+        protected abstract static String SQL_SELECT;
+        protected static String SQL_INSERT;
+        protected static String SQL_UPDATE;
+        protected static String SQL_DELETE;
+        #endregion
+
+        #region CRUD
+        public abstract int Insert(T t, DatabaseProxy pDb = null);
+
+        public abstract int Update(T t, DatabaseProxy pDb = null);
+
+        public abstract Collection<T> Select(DatabaseProxy pDb = null);
+
+        public abstract int Delete(T t, DatabaseProxy pDb = null);
+       
+        #endregion
+
+        #region Operations
+        protected abstract void PrepareCommand(SqlCommand command, T t);
+
+        protected abstract Collection<T> Read(SqlDataReader reader);
+        #endregion
+
+        #region Connect/Disconnect
+        protected void Connect(DatabaseProxy pDb = null)
+        {
+            if (pDb == null)
+            {
+                db = new Database();
+                db.Connect();
+            }
+            else
+            {
+                db = (Database)pDb;
+            }
+        }
+
+        protected void Disconnect(DatabaseProxy pDb = null)
+        {
+            if (pDb == null)
+            {
+                pDb.Close();
+            }
+        }
+        #endregion
+
+    }
+}   
--- learningStore/learningStore/tables/HodnoceniTable.cs
+++ learningStore/learningStore/tables/HodnoceniTable.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
 namespace learningStore.tables
 {
-    class HodnoceniTable
+    public class HodnoceniTable : TableProxy<Hodnoceni>
     {
 
+        public HodnoceniTable()
+            : base()
+        {
+            
+        }
+
+        public override int Insert(Hodnoceni t, database.proxy.DatabaseProxy pDb = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Update(Hodnoceni t, database.proxy.DatabaseProxy pDb = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Collection<Hodnoceni> Select(database.proxy.DatabaseProxy pDb = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Delete(Hodnoceni t, database.proxy.DatabaseProxy pDb = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void PrepareCommand(System.Data.SqlClient.SqlCommand command, Hodnoceni t)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Collection<Hodnoceni> Read(System.Data.SqlClient.SqlDataReader reader)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
